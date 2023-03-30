9920d2717ee4debfd5c67a732e6a5bec46b61057
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2023-03-30 20:31:34 +0100

    Added logic to calendar

--- lib/pages/bookings_page.dart
+++ lib/pages/bookings_page.dart
@@ -1,4 +1,5 @@
 import 'package:bodybuddiesapp/utils/colors.dart';
+import 'package:bodybuddiesapp/utils/constants.dart';
 import 'package:bodybuddiesapp/widgets/booking_widget.dart';
 import 'package:bodybuddiesapp/widgets/medium_text_widget.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +14,25 @@ class BookingsPage extends StatefulWidget {
 }
 
 class _BookingsPageState extends State<BookingsPage> {
+  List<Widget> dates = [];
+  final _currentDate = DateTime.now();
+
+  @override
+  void initState() {
+    initDates(context);
+    super.initState();
+  }
+
+  void initDates(BuildContext context) {
+    if (dates.isEmpty)
+      // ignore: curly_braces_in_flow_control_structures
+      for (int i = 0; i < 16; i++) {
+        final date = _currentDate.add(Duration(days: i));
+        dates.add(dateWidget(
+            date.day.toString(), daysOfWeek[date.weekday - 1].substring(0, 3)));
+      }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -28,16 +48,15 @@ class _BookingsPageState extends State<BookingsPage> {
             SizedBox(
               height: Dimensions.height10,
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                dateWidget("12", "Wed"),
-                dateWidget("13", "Thurs"),
-                dateWidget("14", "Fri"),
-                dateWidget("15", "Sat"),
-                dateWidget("16", "Sun"),
-                dateWidget("17", "Mon"),
-              ],
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: Dimensions.width15),
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: dates.map((e) => e).toList(),
+                ),
+              ),
             ),
             // BookingWidget(),
             // BookingWidget(),
@@ -51,7 +70,7 @@ class _BookingsPageState extends State<BookingsPage> {
 
   Widget dateWidget(String day, String weekDay) {
     return Padding(
-      padding: EdgeInsets.symmetric(horizontal: Dimensions.width10/2.5),
+      padding: EdgeInsets.symmetric(horizontal: Dimensions.width10 / 2.5),
       child: SizedBox(
         width: Dimensions.width10 * 4,
         height: Dimensions.height10 * 5.5,
--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -1,7 +1,7 @@
 String ASSETS = "assets/";
 
 List months = ['Jan', 'Feb', 'Mar', 'Apr', 'May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
-
+List<String> daysOfWeek = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
 List<String> nutrientAssets = ["macro.png", "fats.png", "proteins.png", "calories.png", "carbs.png", "cheat_meal.png"];
 List<String> nutrientTypes = ["Macro Nutrients", "Fats", "Proteins", "Calories", "Carbohydrates", "Cheat Meal"];
 
