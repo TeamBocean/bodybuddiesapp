b1c09b78ebb20e20b9ab148e2189fdd6b2520bf9
Mahmoud Almahroum <mahmoud.al808@gmail.com>
2024-03-21 00:34:20 +0000

    Added booking id and option to update booking name

--- lib/models/booking.dart
+++ lib/models/booking.dart
@@ -1,17 +1,20 @@
 class Booking {
+  String id;
   String bookingName;
   String time;
   String date;
   double price;
 
   Booking(
-      {required this.bookingName,
+      {this.id = '',
+      required this.bookingName,
       required this.price,
       required this.time,
       required this.date});
 
-  factory Booking.fromJson(var data) {
+  factory Booking.fromJson(var data, String id) {
     return Booking(
+        id: id,
         bookingName: data['name'],
         price: data['price'].toDouble(),
         time: data['time'],
